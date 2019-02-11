c0aaf61e5d6c1f854d337598c841334f5ad8ac39
NassimElMard <nassim.elmardi@gmail.com>
2019-02-11 23:49:16 +0100

    Updated Detail action method to use action result types

--- src/GalleryBookApp/Controllers/ComicBooksController.cs
+++ src/GalleryBookApp/Controllers/ComicBooksController.cs
@@ -8,5 +8,14 @@ namespace GalleryBookApp.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return Redirect("/");
+            }
+            return Content("this is response from the detail action method!");
+        }
+            
     }
 }
