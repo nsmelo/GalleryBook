617f5a3eecdff3cbfc638ff97c20df54c4736264
NassimElMard <nassim.elmardi@gmail.com>
2019-02-12 10:31:29 +0100

    Updated Comic Book and artist Model

---
+++ src/GalleryBookApp/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GalleryBookApp.Models
+{
+    public class Artist
+    {
+        public string role { get; set; }
+        public string name { get; set; }
+    }
+}
